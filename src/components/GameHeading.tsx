c38f004456e7a5dda1320da8ff0d687ed7d652f0
Michael Frieze <michaelfrieze@icloud.com>
2023-07-06 15:03:05 -0400

    refactored by creating genre and platform lookup hooks, fixed initial data react-query issue, and organized imports

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,20 +1,15 @@
 import { Heading } from '@chakra-ui/react';
 import { GameQuery } from '../App';
-import useGenres from '../hooks/useGenres';
-import usePlatforms from '../hooks/usePlatforms';
+import useGenre from '../hooks/useGenre';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genresData } = useGenres();
-  const genre = genresData?.results.find((g) => g.id === gameQuery.genreId);
-
-  const { data: platformsData } = usePlatforms();
-  const platform = platformsData?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
+  const genre = useGenre(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
 
   const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
